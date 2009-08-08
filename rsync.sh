d6990fee7900bff0b777771e9c84d92936650dc4
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-08-08 23:37:35 +0530

    deleting unwanted files

--- rsync.sh
+++
@@ -1,4 +0,0 @@
-rsync --verbose  --progress --stats --compress --rsh=/usr/bin/ssh \
-      --recursive --times --perms --links --delete --update  \
-      --exclude "*bak" --exclude "*~" \
-      /sda5/smc/smc/silpadev/* smcweb@smc.org.in:/home/smcweb/smc.org.in/silpadev/
--- testtemplate.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from common import *
-response=SilpaResponse()
-response.setBreadcrumb("TestBreadCrumb")
-response.setForm("TestContent")
-response.setErrorMessage("TestError")	
-response.setSuccessMessage("TestSuccess")		
-print response
