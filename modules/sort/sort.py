1bceda60b17499d429f7ea894f47085dd10c7389
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-08-20 23:05:56 +0530

    Adding two algorithms for sorting default UCA and silpa(patched version)

--- modules/sort/sort.py
+++ modules/sort/sort.py
@@ -7,18 +7,21 @@ from pyuca import Collator
 class Sort(SilpaModule):
 	def __init__(self):
 		self.template=os.path.join(os.path.dirname(__file__), "sort.html")
-		self.collator = Collator(os.path.join(os.path.dirname(__file__), "allkeys.txt"))
+		self.silpacollator = Collator(os.path.join(os.path.dirname(__file__), "allkeys-silpa.txt"))
+		self.ucacollator = Collator(os.path.join(os.path.dirname(__file__), "allkeys.txt"))
 		
 	@ServiceMethod	
 	def sort(self, text):
 		words = text.split()
-		sorted_words = sorted(words, key=self.collator.sort_key) 
-		return dumps(list(sorted_words))
+		sorted_words= {}
+		sorted_words['SILPA'] = sorted(words, key=self.silpacollator.sort_key) 
+		sorted_words['UCA'] = sorted(words, key=self.ucacollator.sort_key) 
+		return dumps(sorted_words)
 
 	def get_module_name(self):
 		return "Sort"
 	def get_info(self):
-		return 	"Sorts a set of words linguistically"	
+		return 	"Sorts a set of words linguistically."	
 
 def getInstance():
 	return Sort()
