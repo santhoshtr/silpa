8f30fe6b469b9ea3f217e4382544c12176d484ea
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-09-02 22:28:22 +0530

    Minor formatting fixes

--- jsonrpc/serviceHandler.py
+++ jsonrpc/serviceHandler.py
@@ -25,137 +25,132 @@ from jsonrpc import dumps
 from jsonrpc import loads
 from utils import *
 
-
-def ServiceMethod(fn):
-	fn.IsServiceMethod = True
-	return fn
-
 class ServiceException(Exception):
-	pass
+    pass
 
 class ServiceRequestNotTranslatable(ServiceException):
-	pass
+    pass
 
 class BadServiceRequest(ServiceException):
-	pass
+    pass
 
 class ServiceMethodNotFound(ServiceException):
-	def __init__(self, name):
-		self.methodName = name
+    def __init__(self, name):
+        self.methodName = name
 
 class ServiceHandler(object):
 
-	def __init__(self, service):
-		self.service = service
-	
-	def handleRequest(self, json):
-		err = None
-		meth=None
-		result = None
-		id_ = ''
-		args = None
-		try:
-			req = self.translateRequest(json)
-		except ServiceRequestNotTranslatable, e:
-			err = e
-			req = {'id':id_}
+    def __init__(self, service):
+        self.service = service
+    
+    def handleRequest(self, json):
+        err = None
+        meth=None
+        result = None
+        id_ = ''
+        args = None
+        try:
+            req = self.translateRequest(json)
+        except ServiceRequestNotTranslatable, e:
+            err = e
+            req = {'id':id_}
 
-		if err == None:
-			try:
-				id_ = req['id']
-				methName = req['method']
-				try:
-					args = req['params']	
-				except:
-					pass		
-			except:
-				err = BadServiceRequest(json)
-		module_instance=None		
-		if err == None:
-			try:
-				meth = self.locate(methName)
-			except Exception, e:
-				err = e
+        if err == None:
+            try:
+                id_ = req['id']
+                methName = req['method']
+                try:
+                    args = req['params']    
+                except:
+                    pass        
+            except:
+                err = BadServiceRequest(json)
+        module_instance=None        
+        if err == None:
+            try:
+                meth = self.locate(methName)
+            except Exception, e:
+                err = e
 
-		if err == None:
-			try:
-				result = self.call(meth, args)
-			except Exception, e:
-				err = e
+        if err == None:
+            try:
+                result = self.call(meth, args)
+            except Exception, e:
+                err = e
 
-		resultdata = self.translateResult(result, err, id_)
-		return resultdata
+        resultdata = self.translateResult(result, err, id_)
+        return resultdata
 
-	def translateRequest(self, data):
-		try:	
-			req = loads(data)
-		except:
-			raise ServiceRequestNotTranslatable(data)
-		return req
-	 
-	def locate(self, name):
-		meth=None
-		try:
-			if name.startswith("system."):
-				return  getattr(self, name.split(".")[1])
-			module_manager = ModuleManager()
-			modules = module_manager.getAllModules()
-			for module in modules:
-				for key in dir(module):
-					try:
-						method = getattr(module, key)
-						if getattr(method, "IsServiceMethod"):
-							if ("modules."+module.__class__.__name__ + "." + key) == name :
-								meth = method
-								break
-					except AttributeError:
-						pass
-			if meth==None :
-				raise ServiceMethodNotFound(name)
-		except AttributeError:
-			raise ServiceMethodNotFound(name)
-		
-		return  meth
-	def listMethods(self):
-		results = []
-		module_manager = ModuleManager()
-		modules = module_manager.getAllModules()
-		for module in modules:
-			for key in dir(module):
-				method = getattr(module, key)
-				try:
-					if getattr(method, "IsServiceMethod"):
-						results.append("modules."+module.__class__.__name__ + "." + key)
-				except:
-					pass		
-		results.sort()
-		return results
+    def translateRequest(self, data):
+        try:    
+            req = loads(data)
+        except:
+            raise ServiceRequestNotTranslatable(data)
+        return req
+     
+    def locate(self, name):
+        meth=None
+        try:
+            if name.startswith("system."):
+                return  getattr(self, name.split(".")[1])
+            module_manager = ModuleManager()
+            modules = module_manager.getAllModules()
+            for module in modules:
+                for key in dir(module):
+                    try:
+                        method = getattr(module, key)
+                        if getattr(method, "IsServiceMethod"):
+                            if ("modules."+module.__class__.__name__ + "." + key) == name :
+                                meth = method
+                                break
+                    except AttributeError:
+                        pass
+            if meth==None :
+                raise ServiceMethodNotFound(name)
+        except AttributeError:
+            raise ServiceMethodNotFound(name)
+        
+        return  meth
+    def listMethods(self):
+        results = []
+        module_manager = ModuleManager()
+        modules = module_manager.getAllModules()
+        for module in modules:
+            for key in dir(module):
+                method = getattr(module, key)
+                try:
+                    if getattr(method, "IsServiceMethod"):
+                        results.append("modules."+module.__class__.__name__ + "." + key)
+                except:
+                    pass        
+        results.sort()
+        return results
 
-	def call(self, meth, args):
-		_args=None
-		for  arg in args:
-			if arg!='':
-				if _args==None: 
-					_args=[]
-				_args.append(arg)	
-		if _args==None:
-			return meth()	
-		else:	
-			return meth(*_args)
-		
+    def call(self, meth, args):
+        _args=None
+        for  arg in args:
+            if arg!='':
+                if _args==None: 
+                    _args=[]
+                _args.append(arg)   
+        if _args==None:
+            return meth()   
+        else:   
+            return meth(*_args)
+        
 
-	def translateResult(self, rslt, err, id_):
-		if err != None:
-			err = {"name": err.__class__.__name__, "message":err.message}
-			rslt = None
-
-		try:
-			data = dumps({"result":rslt, "id":id_, "error":err})
-		except JSONEncodeException, e:
-			err = {"name": "JSONEncodeException", "message":"Result Object Not Serializable"}
-			data = dumps({"result":None, "id":id_, "error":err})
-			
-		return data
+    def translateResult(self, rslt, err, id_):
+        if err != None:
+            err = {"name": err.__class__.__name__, "message":err.message}
+            rslt = None
+            print err
+        try:
+            data = dumps({"result":rslt, "id":id_, "error":err})
+        except JSONEncodeException, e:
+            err = {"name": "JSONEncodeException", "message":"Result Object Not Serializable"}
+            data = dumps({"result":None, "id":id_, "error":err})
+            
+        return data
 # --------------------------------------------------------------------
 # request dispatcher
 
