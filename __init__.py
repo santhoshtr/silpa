62c13549dabff0639207fc1d039ee606dff37602
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-09-02 22:29:16 +0530

    Changes for standalone python library

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-import settings
+import sys,os
+sys.path.append(os.path.dirname(__file__))
+import common
+import modules
+import utils
+
--- common/silparequest.py
+++ common/silparequest.py
@@ -2,39 +2,40 @@ from cgi import parse_qs
 from Cookie import BaseCookie
 import cgi
 import re
-_QUOTES_RE = re.compile('"(.*)"')
 class SilpaRequest(object):
-	def __init__(self, environ):
-		self._formvalues = self._parse_query(environ)
-		self._cookies = self._parse_cookies(environ)
-		self._environ=environ
-	def getCookie(self, key):
-		try:
-			return self._cookies[key]
-		except:
-			return None	
-		
-	def get(self , key)	:
-		if(self._formvalues.has_key(key)):
-			return self._formvalues[key].value	
-		return self._environ.get(key,None)
-	def _parse_query(self, environ)	:	
-		form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ,  keep_blank_values=1)
-		return form
-	def body(self):	
-		length = int(self._environ.get('CONTENT_LENGTH', '0'))
-		fileobj = self._environ['wsgi.input']
-		return fileobj.read(length).decode("utf-8")
-	def _parse_cookies(self, environ)	:
-		source=environ.get('HTTP_COOKIE', '')
-		vars = {}
-		if source:
-			cookies = BaseCookie()
-			cookies.load(source)
-			for name in cookies:
-				value = cookies[name].value
-				unquote_match = _QUOTES_RE.match(value)
-				if unquote_match is not None:
-					value = unquote_match.group(1)
-				vars[name] = value
-		return vars
+    def __init__(self, environ):
+        if environ:
+            self._formvalues = self._parse_query(environ)
+            self._cookies = self._parse_cookies(environ)
+        self._environ=environ
+    def getCookie(self, key):
+        try:
+            return self._cookies[key]
+        except:
+            return None 
+        
+    def get(self , key) :
+        if(self._formvalues.has_key(key)):
+            return self._formvalues[key].value  
+        return self._environ.get(key,None)
+    def _parse_query(self, environ) :   
+        form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ,  keep_blank_values=1)
+        return form
+    def body(self): 
+        length = int(self._environ.get('CONTENT_LENGTH', '0'))
+        fileobj = self._environ['wsgi.input']
+        return fileobj.read(length).decode("utf-8")
+    def _parse_cookies(self, environ)   :
+        _QUOTES_RE = re.compile('"(.*)"')
+        source=environ.get('HTTP_COOKIE', '')
+        vars = {}
+        if source:
+            cookies = BaseCookie()
+            cookies.load(source)
+            for name in cookies:
+                value = cookies[name].value
+                unquote_match = _QUOTES_RE.match(value)
+                if unquote_match is not None:
+                    value = unquote_match.group(1)
+                vars[name] = value
+        return vars
