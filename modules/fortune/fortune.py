9955d89f1f92d7b6d11ced7a7a584b67b7af9057
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-11-26 22:01:46 +0530

    Fixed the search in quotes

--- modules/fortune/fortune.py
+++ modules/fortune/fortune.py
@@ -5,41 +5,46 @@
 #  Released under the GPLV3+ license
 
 import os,random
+import codecs 
 from common import *
+
 class Fortune(SilpaModule):
     def __init__(self):
         self.template=os.path.join(os.path.dirname(__file__), 'fortune.html')
     
-    def fortunes(self,infile,pattern=None):
+    def fortunes(self, infile, pattern=None):
         """ Yield fortunes as lists of lines """
         quotes = []
+        results = []
         quote = ''
         for line in infile:
-            line = line.decode("utf-8")
+            #line = unicode(line)
             if line == "%\n":
                 quotes.append(quote)
                 quote = ''
             else:
-                quote+=line
-        if(pattern!=None):
+                quote += line 
+        if pattern:
             for quote in  quotes:
-                if(quote.find(pattern) < 0):
-                    quotes.remove(quote)     
+                if quote.find(pattern) >= 0:
+                    results.append(quote)
+            return results 
         return quotes   
         
     @ServiceMethod          
     def fortune(self, database, pattern=None ):
         filename = os.path.join(os.path.dirname(__file__), 'database', database)
+        fortunes_file = codecs. open(filename,encoding='utf-8', errors='ignore')
         """ Pick a random fortune from a file """
-        fortunes_list=self.fortunes(file(filename),pattern)
-        chosen=""
+        fortunes_list = self.fortunes(fortunes_file, pattern)
+        chosen = ""
         if fortunes_list:
-            chosen= random.choice(fortunes_list)
+            chosen = random.choice(fortunes_list)
         return "".join(chosen)
     
     def get_module_name(self):
         return "Fortune Cookies"
     def get_info(self):
-        return  "Get/Search a random Malayalam quote "
+        return  "Get/Search a random quote "
 def getInstance():
     return Fortune()    
