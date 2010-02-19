724ca7a27c7c1ccea2b79dbd97c610c36bf3b33b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2010-02-19 20:35:17 +0530

    Removing dead code

--- common/silparesponse.py
+++ common/silparesponse.py
@@ -9,28 +9,27 @@ class SilpaResponse:
         self.page = Meld(xhtml)     
     def toString(self):
         return str(self.page)
-    def setBreadcrumb(self,navPath):
-        if(navPath):
-            html= "<a href=\"http://smc.org.in/silpa\">Home</a> &gt; "
-            html=html + navPath
-        else:
-            html="<a href=\"http://smc.org.in/silpa\">Home</a>"
-        self.page.breadcrumb = html 
+
     def setForm(self,value):
         if(value):
             self.page.form = value
+
     def setResult(self,value):
         if(value):
             self.page.result= value 
+
     def setErrorMessage(self,value):
         if(value):
             self.page.errormessage = value
+
     def setSuccessMessage(self,value):
         if(value):
             self.page.successmessage = value
+
     def setContent(self, value):
         if value:
             self.page.content = value           
+
     def setFooter(self, value):
         if value:
             self.page.footer = value
