8e88c03f6e0e1168430fa16d591d320b7dc51ae4
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2010-10-10 13:37:04 +0530

    Adding buildout build system

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
+import sys
+import os
+import datetime
 from setuptools import setup, find_packages
-setup(
-    name = "silpa",
-    version = "0.1-20100221",
-    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    include_package_data=True,
-    author = "Santhosh Thottingal",
-    author_email = "santhosh.thottingal@gmail.com",
-    description = "Silpa Indian Language Computing Library",
-    license = "AGPL",
-    keywords = "nlp indic",
-    url = "http://smc.org.in/silpa",  
 
-    # could also include long_description, download_url, classifiers, etc.
-)
+name='silpa'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README')
+        + '\n' 
+        )
+
+setup(
+    name=name,
+    version=datetime.datetime.now().strftime("%Y%m%d"),
+    url='http://smc.org.in/silpa',
+    license='AGPL 3.0',
+    description='Indic Language Computing Library',
+    author='Santhosh Thottingal',
+    author_email='santhosh.thottingal@gmail.com',
+    long_description=long_description,
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    package_data = {'': ['doc']},
+    namespace_packages=['silpa'],
+    install_requires=['setuptools'],
+    zip_safe = True,
+    )
