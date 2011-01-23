19de97901f8fe8a532df6fde1f6e5aed6626e28a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2011-01-23 09:54:28 +0530

    removing unwanted pkg_resources dependency

--- src/silpa/__init__.py
+++ src/silpa/__init__.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-__import__('pkg_resources').declare_namespace(__name__)
 import sys,os
 sys.path.append(os.path.dirname(__file__))
 import common
